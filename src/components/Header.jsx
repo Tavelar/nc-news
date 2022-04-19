80812312cbf9bbb22c60b5ae9885964b864b3e4a
Tavelar <tavelor@email.com>
2022-04-19 11:00:33 +0100

    first commit before branch

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <h1>Nc News</h1>
+}
+
+export default Header
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+import {useContext} from 'react'
+
+const Nav = () => {
+    
+    return (
+        <nav>
+            <Link to='/'>Home</Link>
+        </nav>
+    )
+}
+export default Nav
